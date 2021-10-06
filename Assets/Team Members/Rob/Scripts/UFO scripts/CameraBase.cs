a20ce8eb3973b84de35e67fd3f70f8646a52d241
Rob <1021549@student.sae.edu.au>
2021-10-06 14:44:49 +1100

    changing folder QOL mostly for organisation

---
+++ Assets/Team Members/Rob/Scripts/UFO scripts/CameraBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBase : MonoBehaviour
+{
+    public virtual void AssignTarget(Transform target)
+    {
+        
+    }
+}
